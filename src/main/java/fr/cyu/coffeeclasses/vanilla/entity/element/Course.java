784107c55004e289f2f5fd27d5bb8448dfb6108b
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2024-11-22 16:11:17 +0100

    feat: Workaround Course comparison issue in user-edit-content.jsp

--- src/main/java/fr/cyu/coffeeclasses/vanilla/entity/element/Course.java
+++ src/main/java/fr/cyu/coffeeclasses/vanilla/entity/element/Course.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
@@ -76,5 +77,19 @@ public class Course {
 	}
 	public void removeAssessment(Assessment assessment) {
 		assessments.remove(assessment);
+	}
+
+	/* Workaround for a bug */
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null || getClass() != obj.getClass()) return false;
+		Course course = (Course) obj;
+		return id == course.id;
 	}
 }
